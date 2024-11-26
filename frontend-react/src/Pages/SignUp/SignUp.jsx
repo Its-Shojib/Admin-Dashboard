d835b9b180fea096011a50b2fbae1ce750156dae
Md Shojib Hossain <mdshojib922@gmail.com>
2024-11-26 15:10:19 +0600

    Signup Page Completed

--- frontend-react/src/Pages/SignUp/SignUp.jsx
+++ frontend-react/src/Pages/SignUp/SignUp.jsx
@@ -8,13 +8,15 @@ import Swal from 'sweetalert2';
 import { useForm } from "react-hook-form"
 import useAxiosPublic from "../../Hooks/useAxiosPublic";
 import useAuth from "../../Hooks/useAuth";
-
+import animation from '../../assets/Signup.json'
+import Lottie from "lottie-react";
+import SectionTitle from "../../components/SectionTitle";
 const SignUp = () => {
 
     let [showPassword, setShowPassword] = useState(false);
     let axiosPublic = useAxiosPublic();
     let navigate = useNavigate()
-    let {setUser} = useAuth();
+    let { setUser } = useAuth();
 
     const { register, handleSubmit, formState: { errors }, } = useForm();
     const onSubmit = async (data) => {
@@ -25,11 +27,11 @@ const SignUp = () => {
                 password: data?.password,
                 user_role: false
             };
-    
+
             console.log(user);
             let res = await axiosPublic.post('/api/signup', user);
             console.log(res.data);
-    
+
             if (res.data.result) {
                 Swal.fire({
                     position: "center",
@@ -40,18 +42,18 @@ const SignUp = () => {
                 });
                 setUser(res.data.user);
                 localStorage.setItem('access-token', res.data.token);
-                localStorage.setItem("user", JSON.stringify(res.data.user)); 
+                localStorage.setItem("user", JSON.stringify(res.data.user));
                 navigate('/');
             }
         } catch (error) {
             if (error.response?.status === 422) {
                 const validationErrors = error.response.data.errors;
                 console.error("Validation Errors:", validationErrors);
-    
+
                 Swal.fire({
                     icon: "error",
                     title: "Signup Failed",
-                    text: Object.values(validationErrors).flat().join(", "), 
+                    text: Object.values(validationErrors).flat().join(", "),
                 });
             } else {
                 Swal.fire({
@@ -63,64 +65,69 @@ const SignUp = () => {
             }
         }
     };
-    
+
 
 
     return (
-        <div className="flex px-2 justify-center items-center min-h-screen">
-            <div className="bg-gray-400 w-full md:w-4/12 text-center p-10 rounded-lg">
-                <h2 className="text-3xl font-bold mb-2">Signup Now!</h2>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="relative">
-                        <p className="text-left text-lg font-semibold">User Name</p>
-                        <BsFillPersonFill className="absolute bottom-4 left-2"></BsFillPersonFill>
-                        <input className="w-full p-2 pl-7 text-black rounded-lg my-1"
-                            type="text"
-                            {...register("name", { required: true })}
-                            placeholder="Type your name"
-                        />
-                        {errors?.name && <span className='text-red-600'>Name is required</span>}
-                    </div>
-                    <hr className="my-2" />
-                    <div className="relative">
-                        <p className="text-left text-lg font-semibold">User Email</p>
-                        <AiOutlineMail className="absolute bottom-4 left-2"></AiOutlineMail>
-                        <input className="w-full p-2 pl-7 text-black rounded-lg my-1"
-                            type="email"
-                            {...register("email", { required: true })}
-                            placeholder="Type your email"
-                        />
-                        {errors?.email && <span className='text-red-600'>Email is required</span>}
-                    </div>
-                    <hr className="my-2" />
-                    <div className="relative">
-                        <p className="text-left text-lg font-semibold">Password</p>
-                        <RiLockPasswordFill className="absolute bottom-4 left-2"></RiLockPasswordFill>
-                        <input className="w-full p-2 pl-6 text-black rounded-lg my-1"
-                            type={showPassword ? 'text' : 'password'}
-                            {...register("password", {
-                                required: true,
-                                minLength: 6,
-                                maxLength: 20,
-                            })}
-                            placeholder="Type your password"
-                        />
-                        {errors.password?.type === "required" && (
-                            <span className='text-red-600'>Password is required</span>
-                        )}
-                        {errors?.password?.type === 'minLength' && <span className='text-red-600'>Password must be 6 character long</span>}
-                        {errors?.password?.type === 'maxLength' && <span className='text-red-600'>Password must be less than 20 character</span>}
-                        <span onClick={() => setShowPassword(!showPassword)} className="absolute right-3 bottom-4">{showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}</span>
-                    </div>
+        <div>
+            <SectionTitle title={'Signup Now'} subtitle={'New user?'} />
+            <div className="flex flex-col md:flex-row-reverse px-2 justify-center items-center w-full md:w-[900px] mx-auto">
+                <div className="bg-gray-400 text-center p-10 rounded-lg flex-1">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="relative">
+                            <p className="text-left text-lg font-semibold">User Name</p>
+                            <BsFillPersonFill className="absolute bottom-4 left-2"></BsFillPersonFill>
+                            <input className="w-full p-2 pl-7 text-black rounded-lg my-1"
+                                type="text"
+                                {...register("name", { required: true })}
+                                placeholder="Type your name"
+                            />
+                            {errors?.name && <span className='text-red-600'>Name is required</span>}
+                        </div>
+                        <hr className="my-2" />
+                        <div className="relative">
+                            <p className="text-left text-lg font-semibold">User Email</p>
+                            <AiOutlineMail className="absolute bottom-4 left-2"></AiOutlineMail>
+                            <input className="w-full p-2 pl-7 text-black rounded-lg my-1"
+                                type="email"
+                                {...register("email", { required: true })}
+                                placeholder="Type your email"
+                            />
+                            {errors?.email && <span className='text-red-600'>Email is required</span>}
+                        </div>
+                        <hr className="my-2" />
+                        <div className="relative">
+                            <p className="text-left text-lg font-semibold">Password</p>
+                            <RiLockPasswordFill className="absolute bottom-4 left-2"></RiLockPasswordFill>
+                            <input className="w-full p-2 pl-6 text-black rounded-lg my-1"
+                                type={showPassword ? 'text' : 'password'}
+                                {...register("password", {
+                                    required: true,
+                                    minLength: 6,
+                                    maxLength: 20,
+                                })}
+                                placeholder="Type your password"
+                            />
+                            {errors.password?.type === "required" && (
+                                <span className='text-red-600'>Password is required</span>
+                            )}
+                            {errors?.password?.type === 'minLength' && <span className='text-red-600'>Password must be 6 character long</span>}
+                            {errors?.password?.type === 'maxLength' && <span className='text-red-600'>Password must be less than 20 character</span>}
+                            <span onClick={() => setShowPassword(!showPassword)} className="absolute right-3 bottom-4">{showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}</span>
+                        </div>
 
-                    <button
-                        className="bg-gradient-to-r from-violet-500 to-fuchsia-500 w-full py-2 text-white font-semibold text-lg rounded-xl mt-5" type="submit">
-                        Signup</button>
+                        <button
+                            className="bg-gradient-to-r from-violet-500 to-fuchsia-500 w-full py-2 text-white font-semibold text-lg rounded-xl mt-5" type="submit">
+                            Signup</button>
 
-                </form>
-                <div className="flex justify-between mt-3">
-                    <span>Already have an account?</span>
-                    <Link className="underline text-base text-blue-600" to='/login'>Login now</Link>
+                    </form>
+                    <div className="flex justify-between mt-3">
+                        <span>Already have an account?</span>
+                        <Link className="underline text-base text-blue-600" to='/login'>Login now</Link>
+                    </div>
+                </div>
+                <div className="">
+                    <Lottie animationData={animation} loop={true} />
                 </div>
             </div>
         </div>
