39c49e16b8b1590ab31eb6ee916b8fdbc250e42b
Md Shojib Hossain <mdshojib922@gmail.com>
2024-11-26 10:17:41 +0600

    Navbar Updated

--- frontend-react/src/Layout/Layout.jsx
+++ frontend-react/src/Layout/Layout.jsx
@@ -1,9 +1,10 @@
+import Navbar from "../components/Navbar";
 
 
 const Layout = () => {
     return (
         <div className="container px-2 mx-auto">
-            <p>Hi! I am Layout </p>
+            <Navbar />
         </div>
     );
 };
