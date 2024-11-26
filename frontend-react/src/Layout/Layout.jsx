0e8017bb481f7b71a918b2020b49a79abd6e0a31
Md Shojib Hossain <mdshojib922@gmail.com>
2024-11-26 10:35:19 +0600

    Routes Added

--- frontend-react/src/Layout/Layout.jsx
+++ frontend-react/src/Layout/Layout.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
 
@@ -5,6 +6,7 @@ const Layout = () => {
     return (
         <div className="container px-2 mx-auto">
             <Navbar />
+            <Outlet />
         </div>
     );
 };
--- frontend-react/src/Routes/Routes.jsx
+++ frontend-react/src/Routes/Routes.jsx
@@ -1,14 +1,30 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout/Layout";
+import Products from "../Pages/Products/Products";
+import About from "../Pages/About/About";
+import Contact from "../Pages/Contact/Contact";
+import Home from "../Pages/Home/Home";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Layout />,
-        children:[
+        children: [
+            {
+                index: true,
+                element: <Home></Home>,
+            },
             {
                 path: "/products",
-                element: <h2>Orders Page</h2>,
+                element: <Products />,
+            },
+            {
+                path: "/about",
+                element: <About />,
+            },
+            {
+                path: "/contact",
+                element: <Contact />,
             }
         ]
     },
