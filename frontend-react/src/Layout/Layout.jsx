04ca118910bd73bd3f407f6d091684f42d924efe
Md Shojib Hossain <mdshojib922@gmail.com>
2024-11-26 17:01:00 +0600

    Navbar and Footer Updated

--- frontend-react/src/Layout/Layout.jsx
+++ frontend-react/src/Layout/Layout.jsx
@@ -5,10 +5,10 @@ import Footer from "../components/Footer";
 
 const Layout = () => {
     return (
-        <div className="w-full px-2 mx-auto">
+        <div className="w-full px-2 md:px-0 mx-auto">
             <Navbar />
             <Outlet />
-            <Footer />
+            <div className="absolute bottom-0 w-full"><Footer /></div>
         </div>
     );
 };
--- frontend-react/src/components/Footer.jsx
+++ frontend-react/src/components/Footer.jsx
@@ -5,32 +5,10 @@ const Footer = () => {
 
     return (
         <div className='mt-20'>
-            <footer className="footer grid grid-cols-2 md:grid-cols-3 mx-auto p-10 bg-[#14354d] text-white">
-                <nav>
-                    <h6 className="footer-title">Services</h6>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
-                </nav>
-                <nav>
-                    <h6 className="footer-title">Company</h6>
-                    <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
-                </nav>
-                <nav>
-                    <h6 className="footer-title">Legal</h6>
-                    <a className="link link-hover">Terms of use</a>
-                    <a className="link link-hover">Privacy policy</a>
-                    <a className="link link-hover">Cookie policy</a>
-                </nav>
-            </footer>
             <footer className="footer px-10 py-2 bg-[#14354d] text-white">
                 <aside className="items-center grid-flow-col">
                     <img className='w-16 h-16' src={img} alt="" />
-                    <p>MiniBazaar Ltd. <br />Providing reliable Service since 2024</p>
+                    <p>MiniBazaar Ltd. <br />Providing reliable Service since {new Date().getFullYear()}</p>
                 </aside>
                 <nav className="md:place-self-center md:justify-self-end">
                     <div className="grid grid-flow-col gap-4">
